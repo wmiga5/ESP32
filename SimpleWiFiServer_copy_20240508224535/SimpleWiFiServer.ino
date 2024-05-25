820da133a9bedeb52a43f1318bf4f57c68330ee1
wmiga5 <wojtek.miga@gmail.com>
2024-05-25 16:16:59 +0200

    Nowy kod na esp32

--- SimpleWiFiServer_copy_20240508224535/SimpleWiFiServer.ino
+++ SimpleWiFiServer_copy_20240508224535/SimpleWiFiServer.ino
@@ -5,13 +5,14 @@
 #include <Bonezegei_DHT11.h>
 
 #include <EEPROM.h>
-
+int SERIAL_NUMBER=1234;
 #define uS_TO_S_FACTOR 1000000
 #define TIME_TO_SLEEP 10
 #define EEPROM_SIZE 128
 Bonezegei_DHT11 dht(18);
 RTC_DATA_ATTR int bootCount = 0;
 String URL = "http://192.168.0.102/szklarnia_1_project/szklarnia_1_test.php";
+//String URL = "http://localhost/szklarnia_1_project/szklarnia_1_test.php";
 String ssid_def = "janpawel3laczylmalesieci";
 String password_def = "kuktas123";
 #define MAX_STRING_LENGTH 31
@@ -43,9 +44,14 @@ void connectWifi(String log, String pass) {
   } else {
     Serial.println("Próba łączenia do sieci default");
     WiFi.begin(ssid_def, password_def);
+    licznik=0;
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
+         licznik++;
+    if (licznik > 10) {
+      break;
+    }
     }
     Serial.print("Connected to: ");
     Serial.println(ssid_def);
@@ -103,19 +109,21 @@ String readStringFromEEPROM(int x) {
 }
 
 void setup() {
-  delay(100);
+   
+  delay(1000);
   EEPROM.begin(EEPROM_SIZE);
 
   bootCount++;
+  Serial.println("bootCount: "+bootCount);
   pinMode(33, INPUT_PULLUP);
   pinMode(27, OUTPUT);
   attachInterrupt(33, buttonpressed, FALLING);
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+ esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   digitalWrite(27, HIGH);
   Serial.begin(9600);
-  /////////////////////////////////////////
-  //Sekcja pierwszego odpalenia
-  if (bootCount == 0) {
+
+  
+  if (bootCount <= 0) {
     String ssid,password;
     Serial.println("Podaj login:");
     Serial.setTimeout(10000);
@@ -126,13 +134,12 @@ void setup() {
     Serial.println(ssid);
     Serial.println(password);
   }
-  ///////////////////////////////////
-  ////////Sprawdzam EEPROM
+
   Serial.println("Odczytany EEPROM\n");
   Serial.println(readStringFromEEPROM(0));
   Serial.println(readStringFromEEPROM(1));
 
-  ////////////
+
   dht.begin();
 
   if (WiFi.status() != WL_CONNECTED) {
@@ -142,7 +149,7 @@ void setup() {
   if (dht.getData()) {
     float temperature = dht.getTemperature();
     int hum = dht.getHumidity();
-  ////////////////////
+  
   ///Sprawdzenie wartości
     Serial.print("Temperature: ");
     Serial.print(temperature);
@@ -151,9 +158,10 @@ void setup() {
     Serial.print("Humidity:");
     Serial.print(hum);
     Serial.println("%");
-  ///////////////////////////////////////////
-  //////Komunikacja ze skryptem PHP
-    String postData = "temperature=" + String(temperature) + "&humidity=" + String(hum);
+ 
+ 
+    String postData = "temperature=" + String(temperature) + "&humidity=" 
+    + String(hum)+"&serial_number="+String(SERIAL_NUMBER)+"&insolation="+String(temperature*hum);
     HTTPClient http;
     http.begin(URL);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -172,7 +180,7 @@ void setup() {
     delay(20);
   }
 
- // delay(3000);
+  delay(3000);
   Serial.println("Lets s sleep");
   digitalWrite(27, LOW);
   esp_deep_sleep_start();
